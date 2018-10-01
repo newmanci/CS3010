058e13678dc5121b86e561afafa43b8095bcd7f5
Ciara Newman <newmanci@tcd.ie>
2018-10-01 19:03:58 +0100

    Test if not in tree

--- LCA/src/LowestCommonAncestorTest.java
+++ LCA/src/LowestCommonAncestorTest.java
@@ -14,32 +14,49 @@ public class LowestCommonAncestorTest {
 
 
 
-@Test // test with only one node in
-public void testSingle()
-{
-	LowestCommonAncestor<Integer> testBST = new LowestCommonAncestor<Integer>();
-	testBST.put(7);
-	assertNull(testBST.search(testBST.root,1,3));
+	@Test // test with only one node in
+	public void testSingle()
+	{
+		LowestCommonAncestor<Integer> testBST = new LowestCommonAncestor<Integer>();
+		testBST.put(7);
+		assertNull(testBST.search(testBST.root,1,3));
 
-}
+	}
 
-@Test // test with multiple nodes
-public void testMultiple()
-{
-	LowestCommonAncestor<Integer> test = new LowestCommonAncestor<Integer>();
-	test.put(9);
-	test.put(1);
-	test.put(4);
-	test.put(19);
-	test.put(9);
-	test.put(13);
-	test.put(3);
-	test.put(2);
-	test.put(15);
-	test.put(14);
-	test.put(16);
+	@Test // test with multiple nodes
+	public void testMultiple()
+	{
+		LowestCommonAncestor<Integer> test = new LowestCommonAncestor<Integer>();
+		test.put(9);
+		test.put(1);
+		test.put(4);
+		test.put(19);
+		test.put(9);
+		test.put(13);
+		test.put(3);
+		test.put(2);
+		test.put(15);
+		test.put(14);
+		test.put(16);
 
-	assertSame(test.search(test.root,19,1).key,9);
-	assertSame(test.search(test.root,19,9).key,9);
-	assertSame(test.search(test.root,14,16).key,15);
-}
+		assertSame(test.search(test.root,19,1).key,9);
+		assertSame(test.search(test.root,19,9).key,9);
+		assertSame(test.search(test.root,14,16).key,15);
+	}
+	@Test // test when left or right are not in the tree
+	public void testNotInTree()
+	{
+
+		LowestCommonAncestor<Integer> test = new LowestCommonAncestor<Integer>();
+		test.put(9);
+		test.put(1);
+		test.put(4);
+		test.put(19);
+		test.put(9);
+		test.put(13);
+		assertNull(test.search(test.root,19,3)); 
+		assertNull(test.search(test.root,1,20));
+		assertNull(test.search(test.root,5,3));
+		assertNull(test.search(test.root,5,5));
+		assertNull(test.search(test.root,20,1));
+	}
