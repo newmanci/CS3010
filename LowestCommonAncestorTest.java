2c06d8ab2ff03f9ed2a63da6bf577e06c81b9055
Ciara Newman <newmanci@tcd.ie>
2018-10-01 12:41:18 +0100

    Trying to link eclipse

--- LowestCommonAncestorTest.java
+++ LowestCommonAncestorTest.java
@@ -4,34 +4,10 @@ import org.junit.Test;
 
 public class LowestCommonAncestorTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	
-	 @Test
-	    public void test1() {
-	        TreeNode root = Helper.getRoot();
-	        TreeNode two = new TreeNode(2);
-	        TreeNode five = new TreeNode(5);
-	        Assert.assertEquals(lowestCommonAncestor(root, two, five), 2);
-	    }
+	@Test // test with no nodes
+	public void testEmpty() {
+		LowestCommonAncestor<Integer> testBST = new LowestCommonAncestor<Integer>();
 
-	    @Test
-	    public void test2() {
-	        TreeNode root = Helper.getRoot();
-	        TreeNode one = new TreeNode(1);
-	        TreeNode nine = new TreeNode(9);
-	        Assert.assertEquals(lowestCommonAncestor(root, one, nine), 6);
-	    }
-
-	    @Test
-	    public void test3() {
-	        TreeNode root = Helper.getRoot();
-	        TreeNode one = new TreeNode(1);
-	        TreeNode five = new TreeNode(5);
-	        Assert.assertEquals(lowestCommonAncestor(root, one, five), 2);
-	    }
-
+		assertNull( testBST.search(testBST.root, 7, 1));
 	}
-
-}
+	
