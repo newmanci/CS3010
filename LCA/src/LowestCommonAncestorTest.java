ea25acc9ae2da6a7f669ae7a183fa70d4abda8d3
Ciara Newman <newmanci@tcd.ie>
2018-10-01 18:55:24 +0100

    Test with only one node

--- LCA/src/LowestCommonAncestorTest.java
+++ LCA/src/LowestCommonAncestorTest.java
@@ -10,5 +10,13 @@ public class LowestCommonAncestorTest {
 
 		assertNull( testBST.search(testBST.root, 7, 1));
 	}
+	@Test // test with only one node in
+	public void testSingle()
+	{
+		LowestCommonAncestor<Integer> testBST = new LowestCommonAncestor<Integer>();
+		testBST.put(7);
+		assertNull(testBST.search(testBST.root,1,3));
+
+	}
 }
 
