a3324b06b4f063709922025aa675e0e1ad64a827
newmanci <43169310+newmanci@users.noreply.github.com>
2018-10-17 19:07:41 +0100

    Defining relevant variables

--- LCA/src/DAG.java
+++ LCA/src/DAG.java
@@ -3,6 +3,10 @@ import java.util.Iterator;
 import java.util.LinkedList;
 
 public class DAG {
+private int[] indegree;        // indegree[v] = indegree of vertex v
+private final int V;           // number of vertices in this digraph
+private int E;                 // number of edges in this digraph
+private ArrayList<Integer>[] adj;    // adj[v] = adjacency list for vertex v
 
 public DagLCA(int V) {
 		if (V < 0) throw new IllegalArgumentException("Number of vertices in a Digraph must be nonnegative");
