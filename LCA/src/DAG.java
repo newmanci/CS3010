f193f56e9092f3949fba7d96f429b6b90308620e
newmanci <43169310+newmanci@users.noreply.github.com>
2018-10-17 19:11:04 +0100

    Addition to first function

--- LCA/src/DAG.java
+++ LCA/src/DAG.java
@@ -8,6 +8,19 @@ private final int V;           // number of vertices in this digraph
 private int E;                 // number of edges in this digraph
 private ArrayList<Integer>[] adj;    // adj[v] = adjacency list for vertex v
 
+
+public boolean isDAG;
+public boolean[] marked; //boolean array for visited vertices
+public boolean[] stack; 
+	
+// For DFS 
+public int[]pre;   
+public int[]post  ;
+public ArrayList<Integer> postorder;
+public ArrayList<Integer> preorder ;
+public int preCounter;
+public int postCounter;
+
 public DagLCA(int V) {
 		if (V < 0) throw new IllegalArgumentException("Number of vertices in a Digraph must be nonnegative");
 		this.V = V;
@@ -19,4 +32,16 @@ public DagLCA(int V) {
 			
 		
 		}
+
+
+		isDAG = true;
+		marked = new boolean[V];
+		stack = new boolean[V];
+		//for DFS
+	    pre    = new int[V()];
+	    post   = new int[V()];
+	    postorder = new ArrayList<Integer>();
+	    preorder  = new ArrayList<Integer>();
+	    preCounter=0;
+	    postCounter=0;
 }
