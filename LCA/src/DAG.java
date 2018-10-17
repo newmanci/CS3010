ab1be0d5dca75d2bdb422e86c7baefd4bead2ef8
newmanci <43169310+newmanci@users.noreply.github.com>
2018-10-17 19:03:49 +0100

    Implementing first function

--- LCA/src/DAG.java
+++ LCA/src/DAG.java
@@ -4,4 +4,15 @@ import java.util.LinkedList;
 
 public class DAG {
 
+public DagLCA(int V) {
+		if (V < 0) throw new IllegalArgumentException("Number of vertices in a Digraph must be nonnegative");
+		this.V = V;
+		this.E = 0;
+		indegree = new int[V];
+		adj = (ArrayList<Integer>[]) new ArrayList[V];
+		for (int v = 0; v < V; v++) {
+			adj[v] = new ArrayList<Integer>();
+			
+		
+		}
 }
