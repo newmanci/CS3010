48f8366260f758149d2d97776cbf6b0669302da8
newmanci <43169310+newmanci@users.noreply.github.com>
2018-10-22 08:54:52 +0100

    reverse to navigate to parent nodes

--- LCA/src/DAG.java
+++ LCA/src/DAG.java
@@ -3,25 +3,25 @@ import java.util.Iterator;
 import java.util.LinkedList;
 
 public class DAG {
-private int[] indegree;        // indegree[v] = indegree of vertex v
-private final int V;           // number of vertices in this digraph
-private int E;                 // number of edges in this digraph
-private ArrayList<Integer>[] adj;    // adj[v] = adjacency list for vertex v
+	private int[] indegree;        // indegree[v] = indegree of vertex v
+	private final int V;           // number of vertices in this digraph
+	private int E;                 // number of edges in this digraph
+	private ArrayList<Integer>[] adj;    // adj[v] = adjacency list for vertex v
 
 
-public boolean isDAG;
-public boolean[] marked; //boolean array for visited vertices
-public boolean[] stack; 
-	
-// For DFS 
-public int[]pre;   
-public int[]post  ;
-public ArrayList<Integer> postorder;
-public ArrayList<Integer> preorder ;
-public int preCounter;
-public int postCounter;
+	public boolean isDAG;
+	public boolean[] marked; //boolean array for visited vertices
+	public boolean[] stack; 
 
-public DagLCA(int V) {
+	// For DFS 
+	public int[]pre;   
+	public int[]post  ;
+	public ArrayList<Integer> postorder;
+	public ArrayList<Integer> preorder ;
+	public int preCounter;
+	public int postCounter;
+
+	public DAG(int V) {
 		if (V < 0) throw new IllegalArgumentException("Number of vertices in a Digraph must be nonnegative");
 		this.V = V;
 		this.E = 0;
@@ -29,8 +29,8 @@ public DagLCA(int V) {
 		adj = (ArrayList<Integer>[]) new ArrayList[V];
 		for (int v = 0; v < V; v++) {
 			adj[v] = new ArrayList<Integer>();
-			
-		
+
+
 		}
 
 
@@ -38,13 +38,13 @@ public DagLCA(int V) {
 		marked = new boolean[V];
 		stack = new boolean[V];
 		//for DFS
-	    pre    = new int[V()];
-	    post   = new int[V()];
-	    postorder = new ArrayList<Integer>();
-	    preorder  = new ArrayList<Integer>();
-	    preCounter=0;
-	    postCounter=0;
-}
+		pre    = new int[V()];
+		post   = new int[V()];
+		postorder = new ArrayList<Integer>();
+		preorder  = new ArrayList<Integer>();
+		preCounter=0;
+		postCounter=0;
+	}
 
 	/**
 	 * Returns the number of vertices in this digraph.
@@ -71,7 +71,7 @@ public DagLCA(int V) {
 
 	}
 
-/**
+	/**
 	 * Adds the directed edge v-w to this digraph.
 	 *
 	 * @param  v the tail vertex
@@ -128,3 +128,62 @@ public DagLCA(int V) {
 		}
 		stack[v] = false;
 	}
+
+	public int LCA(int v, int w)
+	{
+		boolean haveLCA =false;
+		//Will be acyclic if reversed so no need to reverse yet
+		isAcyclic();
+		if(!isDAG){
+			return -1;
+		}
+		validateVertex(v);//exception thrown if invalid
+		validateVertex(w);//check if they exist
+		if(E==0)// no edges
+		{
+			return -1;
+		}
+		//is valid to search now reverse
+
+		DAG  reversed = this.reverse();
+		ArrayList<Integer> commonAncestors = new ArrayList<Integer>();
+
+		//Testing DFS first to see if it works       
+		ArrayList<Integer> childVSearch =reversed.bfs(v);
+
+		ArrayList<Integer> childWSearch= reversed.bfs(w);
+		for(int i = 0; i<childVSearch.size(); i++)
+		{
+			for(int t = 0; t<childWSearch.size(); t++)
+			{		
+				if(childVSearch.get(i)==childWSearch.get(t))
+				{ // first occurrence of a match is where the lowest common 
+					commonAncestors.add(childVSearch.get(i));	
+					haveLCA = true;
+				}
+			}
+
+
+		}
+		if(haveLCA)
+			return commonAncestors.get(0);
+		else
+
+			return -1;
+	}
+	//reverse the DAG so we can navigate to parent nodes. th the add edge if thats even allowed
+
+	public DAG reverse() {
+		DAG childToParent = new DAG(V);
+		for (int v = 0; v < V; v++) 
+		{
+
+			for (int w : adj(v)) 
+			{
+				childToParent.addEdge(w, v);
+			}
+		}
+		return childToParent;
+	}
+}
+
