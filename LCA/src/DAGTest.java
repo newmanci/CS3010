01108bf35eb16ddce73aeb919370a0b6502d96eb
newmanci <43169310+newmanci@users.noreply.github.com>
2018-10-24 12:36:33 +0100

    Testing edges and vertices

--- LCA/src/DAGTest.java
+++ LCA/src/DAGTest.java
@@ -1,12 +1,25 @@
 import static org.junit.Assert.*;
-
+import java.util.ArrayList;
 import org.junit.Test;
-
 public class DAGTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testE()
+	{
+		DAG test1 = new DAG(13);
+		assertEquals("testing with no edges",0,test1.E());
+		test1.addEdge(1, 2);
+		assertEquals("testing with no edges",1,test1.E());
+	}
+
+	@Test
+	public void testV()
+	{
+		DAG test1 = new DAG(20);
+		assertEquals("testing V() function", 20, test1.V());
+
+	}
+	
 	}
 
 }
